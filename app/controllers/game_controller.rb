725bc5e3ec2650e726c46d0b8440b6077501feae
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-24 08:17:29 -0400

    updated score out of

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,16 +12,40 @@ class GameController < ApplicationController
         if @quote.politician_id == @politician.id
           flash[:notice] = "Correct! And worth knowing that Politifact rated #{@quote.politician.name}'s quote as '#{@quote.fact_check_rating}.' You can learn more here: #{@quote.source}."
           session[:score]+= 1
+          if session[:count].nil?
+            session[:count] = 0
+            session[:count]+= 1
+          else
+            session[:count]+= 1
+          end
         else
           flash[:notice] = "Wrong! It was #{@quote.politician.name} who said '#{@quote.direct_quote}.' Politifact rated this quote as '#{@quote.fact_check_rating}'."
+          if session[:count].nil?
+            session[:count] = 0
+            session[:count]+= 1
+          else
+            session[:count]+= 1
+          end
         end
         redirect_to :back
       elsif params[:didnt_say_it]
         if @quote.politician_id != @politician.id
           flash[:notice] = "Correct! #{@politician.name} didn't say it – it was #{@quote.politician.name} who said #{@quote.direct_quote}. Politifact rated this quote as '#{@quote.fact_check_rating}'."
           session[:score]+= 1
+          if session[:count].nil?
+            session[:count] = 0
+            session[:count]+= 1
+          else
+            session[:count]+= 1
+          end
         else
           flash[:notice] = "Wrong! #{@politician.name} didn't say it – it was #{@quote.politician.name} who said #{@quote.direct_quote}. Politifact rated this quote as '#{@quote.fact_check_rating}.'"
+          if session[:count].nil?
+            session[:count] = 0
+            session[:count]+= 1
+          else
+            session[:count]+= 1
+          end
         end
         redirect_to :back
       end
@@ -29,6 +53,7 @@ class GameController < ApplicationController
 
     def start_over
       session[:score] = 0
+      session[:count] = 0
       # redirect_to :action => "index"
       redirect_to root_url
     end
--- app/models/question.rb
+++
@@ -1,8 +0,0 @@
-class Question < ActiveRecord::Base
-  belongs_to :game
-end
-
-def quote_question
-  self.quote
-  self.politician 
-end
